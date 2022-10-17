1ce31117772a67a5fb6dc24e511f2b203f748473
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-17 13:20:45 +0600

    onBlur, onChange, onSubmit explore

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import app from './Firebase/Firebase.init';
 
 const auth = getAuth(app);
 
+const handleRegister = (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email, password);
+}
+
+const handleEmailBlur = (event) => {
+  console.log(event.target.value);
+}
+
+const handlePasswordBlur = (event) => {
+  console.log(event.target.value);
+}
+
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type='email' name='email' id='' placeholder='Enter Your Email' />
+      <form onBlur={handleRegister}>
+        <input onChange={handleEmailBlur} type='email' name='email' id='' placeholder='Enter Your Email' required />
         <br/>
-        <input type='password' name='password' id='' placeholder='Enter Your Password' />
+        <input onBlur={handlePasswordBlur} type='password' name='password' id='' placeholder='Enter Your Password' required />
+        <br />
+        <button type='submit'>Register</button>
       </form>
     </div>
   );
--- src/__Steps.js
+++ src/__Steps.js
@@ -3,3 +3,10 @@ Auth Terminology:
 1,Register or sign up(first time create a user)
 2, Log in or sign in(already you have a user account created)
 */
+
+/* 
+Get form value:
+1,Set form onSubmitHandler
+2.Set event parameter
+3,event.target.email.value;
+*/
