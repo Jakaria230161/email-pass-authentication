4d77bd28e5d6e93da977251c43395ca86ff2d048
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-18 22:54:57 +0600

    email pass auth done

--- src/components/LoginBootstrap.js
+++ src/components/LoginBootstrap.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import app from '../Firebase/Firebase.init';
 
-const loginBootstrap = () => {
+const auth = getAuth(app);
+
+
+const LoginBootstrap = () => {
+    const [success, setSuccess] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+
     const handleSubmit = event => {
         event.preventDefault();
         const form = event.target;
-        const email = form.email;
-        const password = form.password;
+        const email = form.email.value;
+        const password = form.password.value;
         console.log(email, password);
 
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess(true);
+            })
+            .catch(error => {
+                console.error("error", error);
+        })
+
     }
+
+    const handleEmailBlur = event => {
+        const email = event.target.value;
+        setUserEmail(email);
+        console.log(email);
+    }
+
+    const handleForgetPassword = () => {
+        if (!userEmail) {
+            alert('please enter your email address')
+            return;
+        }
+        sendPasswordResetEmail(auth, userEmail)
+            .then(() => {
+            alert("Password reset email sent, please check your email")
+            })
+            .catch(error => {
+                console.error("error", error);
+        })
+    }
+
     return (
         <div className='w-50 mx-auto'>
             <h3 className='text-white text-center bg-success rounded'>Please Login</h3>
             <form onSubmit={handleSubmit}>
-                 <div class="mb-3">
-  <label for="formGroupExampleInput" class="form-label">Email : </label>
-  <input type="email" name='email' class="form-control" id="formGroupExampleInput" placeholder="Enter Your Email" required/>
+                 <div className="mb-3">
+  <label htmlFor="formGroupExampleInput" className="form-label">Email : </label>
+  <input onBlur={handleEmailBlur} type="email" name='email' className="form-control" id="formGroupExampleInput" placeholder="Enter Your Email" required/>
 </div>
-<div class="mb-3">
-  <label for="formGroupExampleInput2" class="form-label">Password : </label>
-  <input type="password" name = 'password' class="form-control" id="formGroupExampleInput2" placeholder="Enter Your Password" required/>
+<div className="mb-3">
+  <label htmlFor="formGroupExampleInput2" className="form-label">Password : </label>
+  <input type="password" name = 'password' className="form-control" id="formGroupExampleInput2" placeholder="Enter Your Password" required/>
                 </div>
                 <button className='btn btn-outline-success rounded' variant="primary" type="submit">
         Login
       </button>
             </form>
-            <p><small>New to this website? Please <Link to ='/register'>Register</Link></small></p>
+            {success && <p>successfully login to the account.</p>}
+            <p><small>New to this website? Please <Link to='/register'>Register</Link></small></p>
+            <p><small>Forget Password?<button type="button" onClick={handleForgetPassword} className="btn btn-link">Reset password</button>Please request to reset password.</small></p>
         </div>
 
     );
 };
 
-export default loginBootstrap;
+export default LoginBootstrap;
--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile} from 'firebase/auth'
 import app from '../Firebase/Firebase.init';
 import { Link } from 'react-router-dom';
 
@@ -16,9 +16,10 @@ const RegisterReactBootstrap = () => {
     const handleRegister = event => {
       event.preventDefault();
       const form = event.target;
+      const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-      console.log(email, password);
+      console.log(name, email, password);
        // validate password
         if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
             setPasswordError('Please provide at least two uppercase');
@@ -39,18 +40,43 @@ const RegisterReactBootstrap = () => {
           console.log(user);
           setSuccess(true);
           form.reset();
+          verifyEmail();
+          updateUserName(name)
+
         })
         .catch(error => {
           console.error("error", error);
           setPasswordError(error.message);
+        })
+      
+      const verifyEmail = () => {
+        sendEmailVerification(auth.currentUser)
+          .then(() => {
+          alert("Please check your email and verify email address.")
+        })
+      }
+        
+  }
+  
+  const updateUserName = (name) => {
+    updateProfile(auth.currentUser, {
+      displayName : name
+    })
+      .then(() => {
+      console.log("display name updated")
       })
-        
-        
-    }
+      .catch(error => {
+        console.error("error", error);
+    })
+  }
     return (
         <div className='w-50 mx-auto'>
             <h2 className='text-white bg-primary text-center rounded'>Please Register!!!</h2>
             <Form onSubmit={handleRegister}>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Your name : </Form.Label>
+        <Form.Control type="text" name='name' placeholder="Enter Your Name" required/>
+      </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address : </Form.Label>
         <Form.Control type="email" name='email' placeholder="Enter Your Email" required />
